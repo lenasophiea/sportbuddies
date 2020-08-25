8440ccdf0e7ee14f3a74c77d457c5a6023a10cc9
Mohit Rao <mohitrao.87@gmail.com>
2020-08-25 15:31:27 +0200

    navbar updates and frontend

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'sports#index'
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :sports, only: [:index,:show] do
+  end
 end
