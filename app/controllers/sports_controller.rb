f95b93c28b0a058d95a5c20535bb529ab06cfce0
Amelie Kerne <amelie.kerne@gmail.com>
2020-08-25 18:13:53 +0200

    update search front end on index sport

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -2,6 +2,13 @@ class SportsController < ApplicationController
 
   def index
     @sports = policy_scope(Sport)
+    if params[:query_sport].present?
+      @sports = Sport.search_by_sport_filter(params[:query_sport])
+    elsif params[:query_address].present?
+      @sports = Sport.search_by_sport_filter(params[:query_address])
+    else
+      @sports = Sport.all
+    end
   end
 
   def show
--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,3 +1,9 @@
 class Sport < ApplicationRecord
   has_one_attached :photo
+  include PgSearch::Model
+  pg_search_scope :search_by_sport_filter,
+    against: [ :name, :address ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
