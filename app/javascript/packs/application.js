3d469ca86481127a22bd1b1615e20c5b96543f43
Lena Adam <lena.adam@me.com>
2020-08-26 12:33:13 +0200

    add map feature to sport controller and index view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,12 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initMapbox } from '../packs/init_mapbox';
+
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  setTimeout(() => {
+  initMapbox();
+  },200)
 });
+
