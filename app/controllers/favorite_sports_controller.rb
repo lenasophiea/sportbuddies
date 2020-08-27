c141fb8ee12726795d63da8a271de1c9860d4833
Amelie Kerne <amelie.kerne@gmail.com>
2020-08-27 18:00:04 +0200

    new create destroy a fav sport

--- app/controllers/favorite_sports_controller.rb
+++ app/controllers/favorite_sports_controller.rb
@@ -1,2 +1,31 @@
 class FavoriteSportsController < ApplicationController
+
+  def new
+    @sport = Sport.find(params[:sport_id])
+    @fav = FavoriteSport.new
+  end
+
+  def create
+    @fav = FavoriteSport.new(fav_params)
+    @sport = Sport.find(params[:sport_id])
+    @fav.sport = @sport
+    if @fav.save
+      redirect_to sport_path(@sport)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @fav = FavoriteSport.find(params[:id])
+    @fav.destroy
+    redirect_to sport_path(@fav.sport)
+  end
+
+  private
+
+  def fav_params
+    params.require(:fav).permit(:sport)
+  end
+
 end
