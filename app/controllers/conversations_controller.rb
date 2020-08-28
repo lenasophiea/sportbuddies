d56e8e6e4572ab71dd5b501b5676df2087e0d843
Lena Adam <lena.adam@me.com>
2020-08-28 12:22:59 +0200

    fix profil link to message

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,16 +1,29 @@
 class ConversationsController < ApplicationController
 
   def create
-    @conversation = Conversation.new
-    @sender = current_user
     @receiver = User.find(params[:profile_id])
-    @buddy_request = BuddyRequest.find(params[:buddy_request_id])
-    @conversation.sender = @sender
-    @conversation.receiver = @receiver
-    @conversation.buddy_request = @buddy_request
-    authorize @conversation
-    @conversation.save
-    redirect_to conversation_path(@conversation)
+    @sender = current_user
+    first_case = Conversation.find_by(sender_id: @sender.id, receiver_id: @receiver.id)
+    second_case = Conversation.find_by(sender_id: @receiver.id, receiver_id: @sender.id)
+    if first_case || second_case
+      if first_case
+        authorize first_case
+        redirect_to first_case
+      else
+        authorize second_case
+        redirect_to second_case
+      end
+    else
+      @conversation = Conversation.new
+      authorize @conversation
+      @buddy_request = BuddyRequest.find(params[:buddy_request_id])
+      @conversation.sender = @sender
+      @conversation.receiver = @receiver
+      @conversation.buddy_request = @buddy_request
+      @conversation.save
+      redirect_to conversation_path(@conversation)
+    end
+    @sport = Sport.find(params[:id])
   end
 
   def show
